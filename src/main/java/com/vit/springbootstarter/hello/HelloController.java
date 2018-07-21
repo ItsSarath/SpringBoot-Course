5fff39c4d4e390019807dae8f08188cc86580370
Sarath <bschandra92@gmail.com>
2018-07-21 14:57:36 +0530

    Business service template created

--- src/main/java/com/vit/springbootstarter/hello/HelloController.java
+++ src/main/java/com/vit/springbootstarter/hello/HelloController.java
@@ -1,12 +1,17 @@
 package com.vit.springbootstarter.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.vit.springbootstarter.service.TopicService;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	TopicService topicService;
 	
 	@RequestMapping("/") //for GET request
 	public String sayHello(Model model) {
---
+++ src/main/java/com/vit/springbootstarter/service/TopicService.java
@@ -0,0 +1,8 @@
+package com.vit.springbootstarter.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+}
